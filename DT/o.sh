444a81e8b584fa9114c78f5aa2a8b858e0c8c0a0
kvn61 <126725169+kvn61@users.noreply.github.com>
2024-11-01 13:40:31 +0300

    Add files via upload

---
+++ DT/o.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+KDIR=/home/slava/my/br/buildroot/output/build/linux-6.6.37
+
+gcc -E -nostdinc -I$KDIR/include -x assembler-with-cpp -undef -imacros 3308.h -o $1.tmp  $1 
+./dtc -@ -I dts -O dtb -o ${1%%.*}.dtbo $1.tmp
