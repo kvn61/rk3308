77ea739cdd3e01cb22de4c560531e5dd7d28bc76
kvn61 <126725169+kvn61@users.noreply.github.com>
2024-11-01 13:43:12 +0300

    Delete DT/3308.h

--- DT/3308.h
+++
@@ -1,7 +0,0 @@
-#include <dt-bindings/clock/rk3308-cru.h>
-#include <dt-bindings/gpio/gpio.h>
-#include <dt-bindings/interrupt-controller/arm-gic.h>
-#include <dt-bindings/interrupt-controller/irq.h>
-#include <dt-bindings/pinctrl/rockchip.h>
-#include <dt-bindings/soc/rockchip,boot-mode.h>
-#include <dt-bindings/thermal/thermal.h>
