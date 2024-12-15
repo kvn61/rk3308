9baac8c625b34a840dbcb95b092bc245e32eb9a7
kvn61 <126725169+kvn61@users.noreply.github.com>
2024-12-15 14:49:11 +0300

    Add files via upload

---
+++ i2s/gpio-pin.c
@@ -0,0 +1,20 @@
+// i2s or codec DT
+dsd-gpios = <&gpioX Y GPIO_ACTIVE_HIGH>;
+
+// in code
+struct gpio_desc *dsd_pin;
+
+probe 
+		dsd_pin = devm_gpiod_get_optional(&pdev->dev,
+				"dsd", GPIOD_OUT_LOW);
+		if (IS_ERR(dsd_pin))
+			return PTR_ERR(dsd_pin);
+
+hw_params
+	if( params_format(params) == SNDRV_PCM_FORMAT_DSD_U32_LE ) {
+		gpiod_set_value(dsd_pin, 1);
+		dev_dbg(??->dev, "set enable to 1");
+	} else
+		gpiod_set_value(dsd_pin, 0);
+
+
